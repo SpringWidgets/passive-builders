79dacf69b5bc828077630b9b0390c7d53296ba29
Alexander Lipatov <alexlpt@yandex.ru>
2015-06-20 01:08:36 +0300

    v3: exclude Commando from "passive" builders

--- unit_passive_builders.lua
+++ unit_passive_builders.lua
@@ -1,12 +1,12 @@
 function widget:GetInfo()
     return {
-        name      = "Passive builders v2",
+        name      = "Passive builders v3",
         desc      = "All builders + factories (except commander) are set to Passive mode",
         author    = "[teh]decay",
-        date      = "11 jan 2015",
+        date      = "20 jun 2015",
         license   = "The BSD License",
         layer     = 0,
-        version   = 2,
+        version   = 3,
         enabled   = true  -- loaded by default
     }
 end
@@ -15,6 +15,7 @@ end
 
 -- Changelog:
 -- v2 [teh]decay Fixed bug with rezz bots and spys
+-- v3 [teh]decay exclude Commando from "passive" builders
 
 local CMD_PASSIVE       = 34571
 local spGetMyTeamID     = Spring.GetMyTeamID
@@ -22,8 +23,10 @@ local spGetTeamUnits    = Spring.GetTeamUnits
 local spGetUnitDefID    = Spring.GetUnitDefID
 local spGiveOrderToUnit = Spring.GiveOrderToUnit
 
+local coreCommando = UnitDefNames["commando"]
+
 local function isBuilder(ud)
-    return ud and ud.isBuilder and not ud.canManualFire and ud.canAssist
+    return ud and ud.isBuilder and not ud.canManualFire and ud.canAssist and ud.id ~= coreCommando.id
 end
 
 local function passivateBuilder(unitID)
